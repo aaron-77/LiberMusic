574795b9561bd59f47ed0ec52ee238a43b9095ab
jonathan <jhoni65475@gmail.com>
2021-04-04 17:51:59 -0500

    agregando los dos metodos de agregar (Lista y cancion de lista)

--- Controllers/MSListasDeReproduccionController.cs
+++ Controllers/MSListasDeReproduccionController.cs
@@ -4,12 +4,65 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MSListasDeReproduccion.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MSListasDeReproduccion.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class MSListasDeReproduccionController : ControllerBase
     {
+        private libermusiclistasdereproduccionContext dbcontext;
+
+        public MSListasDeReproduccionController()
+        {
+            dbcontext = new libermusiclistasdereproduccionContext();
+        }
+
+        [HttpPost("CrearLista")]
+        public async Task<ActionResult<Listasdereproduccion>> add([FromBody] Listasdereproduccion listaReproduccion) {
+            if (listaReproduccion == null)
+            {
+                return BadRequest("No has introducido datos");
+            }
+            try
+            {
+                
+                    dbcontext.Entry(listaReproduccion).State = EntityState.Added;
+                    await dbcontext.SaveChangesAsync();
+                    return Created("", listaReproduccion);
+             
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error al registrarte" + "\n" + ex);
+            }
+        }
+        [HttpPost("AgregarCancion")]
+        public async Task<ActionResult<Cancioneslistasdereproduccion>> add([FromBody] Cancioneslistasdereproduccion cancionParaLista)
+        {
+            if (cancionParaLista == null)
+            {
+                return BadRequest("No has introducido datos");
+            }
+            try
+            {
+
+                dbcontext.Entry(cancionParaLista).State = EntityState.Added;
+                await dbcontext.SaveChangesAsync();
+                return Created("", cancionParaLista);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error al registrarte" + "\n" + ex);
+            }
+        }
+
+
+
+
+
     }
 }
